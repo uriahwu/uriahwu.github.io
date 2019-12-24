dc7a71ceb49a58fd95859b5ae3715919843437e6
Uriah Wu <uriah0320@gmail.com>
2019-12-24 10:40:24 +0800

    append a link on the page

--- istaging_webar.js
+++ istaging_webar.js
@@ -1,13 +1,29 @@
-var agent = window.navigator.userAgent,
-    device = new MobileDetect(agent);
+(function() {
+  var agent = window.navigator.userAgent,
+      device = new MobileDetect(agent);
 
-console.log(agent);
-console.log(device.os());
-document.write(device.os());
+  console.log(agent);
+  console.log(device.os());
+  console.log(device.userAgent());
+  
+  var base_url = "uriahwu.github.io";
+  
+  item_name = document.getElementById('');
 
-if( device.os() == "AndroidOS" ) {
-    window.location.replace("intent://bamboowand.github.io/scene.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;");
-} else {
-    window.location.href = "https://bamboowand.github.io/coffee_machine.usdz";
-}
+  android_link = "intent://" + base_url + "/coffee_machine.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;";
+  ios_link = "https://" + base_url + "/coffee_machine.usdz";
+  
+  function createLink(os) {
+    var link = document.createElement("a");
+    link.innerHTML = "CLICK ME";
+    if( os == "AndroidOS" ) {
+      link.setAttribute("href", android_link);
+    } else if (os == "iOS" ) {
+      link.setAttribute("href", ios_link);
+    }
+    var target = document.getElementById("web_ar");
+    target.appendChild(link);
+  }
 
+  createLink(device.os());
+})()
