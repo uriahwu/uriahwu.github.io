1608e93f92e1d79e8b65c72071bbdba8e3ed2cf1
Uriah Wu <uriah0320@gmail.com>
2019-12-24 11:44:20 +0800

    remove useless code

--- istaging_webar.js
+++ istaging_webar.js
@@ -2,20 +2,18 @@
   var agent = window.navigator.userAgent,
       device = new MobileDetect(agent);
 
-  console.log(agent);
-  console.log(device.os());
-  console.log(device.userAgent());
-  
   var base_url = "uriahwu.github.io";
   
   item_name = document.getElementById('');
 
-  android_link = "intent://" + base_url + "/coffee_machine.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;";
+  android_link = "intent://uriahwu.github.io/coffee_machine.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;";
   ios_link = "https://" + base_url + "/coffee_machine.usdz";
   
   function createLink(os) {
     var link = document.createElement("a");
     link.innerHTML = "CLICK ME";
+    link.setAttribute("class", "button");
+    
     if( os == "AndroidOS" ) {
       link.setAttribute("href", android_link);
     } else if (os == "iOS" ) {
